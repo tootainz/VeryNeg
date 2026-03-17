109631d77ea2587c967f4dd60cb776b66f7de2d7
Joonatan Koponen <j.copoin@gmail.com>
2026-03-17 15:39:16 +0200

    Got rmlui working finally after a long and irritating fight

--- main.cpp
+++ main.cpp
@@ -4,8 +4,15 @@
 
 #include <OpenImageIO/imageio.h>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
+
+#include <RmlUi/Core.h>
+#include <RmlUi_Platform_SFML.h>
+#include <RmlUi_Renderer_GL2.h>
+#include "rmlui-backend/RmlUi_Backend.h"
 
 #include "Negative/Negative.hpp"
 #include "View/View.hpp"
@@ -20,6 +27,35 @@ int main() {
     Model model;
     View view(window);
     Controller controller(window, view, model);
+
+    Backend::Initialize("VeryNeg", 800, 800, true);
+
+    Rml::SetRenderInterface(Backend::GetRenderInterface());
+    Rml::SetSystemInterface(Backend::GetSystemInterface());
+
+
+    if (!Rml::Initialise()) {
+        std::cout << "RmlUi failed to initialise\n";
+        return -1;
+    }
+
+    Rml::Context* context = Rml::CreateContext("default", Rml::Vector2i(800, 800));
+    context->SetDimensions(Rml::Vector2i(800, 800));
+    bool success = Rml::LoadFontFace("assets/oceert_pixel.otf");
+    Rml::ElementDocument* document = context->LoadDocument("assets/hello.rml");
+    if (document)
+        document->Show();
+
+    // MAIN LOOP
+    while (Backend::ProcessEvents(context))
+    {
+        Backend::BeginFrame();
+
+        context->Update();
+        context->Render();   // REQUIRED
+
+        Backend::PresentFrame();
+    }
 
     // MAIN LOOP
     controller.mainLoop();
